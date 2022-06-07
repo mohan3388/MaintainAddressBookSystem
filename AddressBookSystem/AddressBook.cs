20a8e8b708186a7d82b10818784a314a4f43b665
mohan338 <sahumohan338@gmail.com>
2022-06-07 11:13:03 +0530

    [mohan]add:Add contact to Address book

---
+++ AddressBookSystem/AddressBook.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    public class AddressBook
+    {
+        List<Contact> address = new List<Contact>();
+        public AddressBook()
+        {
+            Contact address1 = new Contact()
+            {
+                FirstName = Console.ReadLine(),
+                LastName = Console.ReadLine(),
+                Address = Console.ReadLine(),
+                City = Console.ReadLine(),
+                State = Console.ReadLine(),
+                Email = Console.ReadLine(),
+                Pincode = Convert.ToInt32(Console.ReadLine()),
+                Mobile = Convert.ToInt64(Console.ReadLine()),
+            };
+            Contact address2 = new Contact()
+            {
+                FirstName = Console.ReadLine(),
+                LastName = Console.ReadLine(),
+                Address = Console.ReadLine(),
+                City = Console.ReadLine(),
+                State = Console.ReadLine(),
+                Email = Console.ReadLine(),
+                Pincode = Convert.ToInt32(Console.ReadLine()),
+                Mobile = Convert.ToInt64(Console.ReadLine()),
+            };
+            address.Add(address1);
+            address.Add(address2);
+        }
+        public void AddcontactToAddressbook(Contact contact)
+        {
+            address.Add(contact);
+        }
+        public void Display()
+        {
+            foreach(var contact in address)
+            {
+                Console.WriteLine(contact.FirstName+" "+contact.LastName+" "+contact.Address+" "+contact.City+" "+contact.State+" "+contact.Email+" "+contact.Pincode+" "+contact.Mobile);
+            }
+        }
+    }
+}
---
+++ AddressBookSystem/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using AddressBookSystem;
+
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        Contact Addresbook = new Contact();
+        Console.WriteLine("Enter details");
+        Addresbook.FirstName = Console.ReadLine();
+        Addresbook.LastName = Console.ReadLine();
+        Addresbook.Address = Console.ReadLine();
+        Addresbook.City = Console.ReadLine();
+        Addresbook.State = Console.ReadLine();
+        Addresbook.Email= Console.ReadLine();
+        Addresbook.Pincode = Convert.ToInt32(Console.ReadLine());
+        Addresbook.Mobile = Convert.ToInt64(Console.ReadLine());
+        Console.WriteLine(Addresbook.FirstName+" "+Addresbook.LastName+" "+Addresbook.Address+" "+Addresbook.City+" "+Addresbook.State+" "+Addresbook.Email+" "+Addresbook.Pincode+" "+Addresbook.Mobile);
+    }
+}
